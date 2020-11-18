f9934b23b5336158505149d8f48f534495191f49
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-17 19:21:00 -0800

    Render blog posts in FlatList

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,12 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={[1, 2, 3]}>
+  const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' }
+  ];
+
+  return <BlogContext.Provider value={blogPosts}>
     {children}
   </BlogContext.Provider>
 };
