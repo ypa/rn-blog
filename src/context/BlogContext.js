95a35824c1201e5105427631ace939f007ab6e21
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-21 19:15:34 -0800

    Clean up add_blogpost action reducer as it's no longer needed

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,15 +13,6 @@ const blogReducer = (state, action) => {
       });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
     default:
       return state;
   }
